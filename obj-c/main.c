bafc4d55f32a9b3f33d7605db335fd24d53eb646
waihoyu <13701343809@qq.com>
2019-10-16 02:59:53 +0800

    Initial Commit

---
+++ obj-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  obj-c
+//
+//  Created by Mac on 10/16/19.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
